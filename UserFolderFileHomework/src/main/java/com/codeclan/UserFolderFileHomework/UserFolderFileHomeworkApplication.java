f0d5b8ac897132ca4157d0de327b0e4e8dd04cb0
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-10-22 21:21:07 +0100

    initial build

---
+++ UserFolderFileHomework/src/main/java/com/codeclan/UserFolderFileHomework/UserFolderFileHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.UserFolderFileHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFolderFileHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFolderFileHomeworkApplication.class, args);
+	}
+
+}
---
+++ UserFolderFileHomework/src/test/java/com/codeclan/UserFolderFileHomework/UserFolderFileHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.UserFolderFileHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFolderFileHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
